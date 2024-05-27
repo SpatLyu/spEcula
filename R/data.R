12fe86aaf47f286120dda942fea32952803fea5e
SpatLyu <lyu.geosocial@gmail.com>
2024-05-28 01:19:10 +0800

    modify zn data and gos function

--- R/data.R
+++ R/data.R
@@ -1,20 +1,22 @@
 #' @title Spatial datasets of trace element Zn.
 #'
-#' @description Spatial datasets of trace element Zn.
+#' @description Spatial datasets of trace element Zn,modified from `geosimilarity`
+#' package.
 #'
 #' @name zn
-#' @format \code{zn}: A data frame of trace element Zn with 894 rows and 12 variables
+#' @format \code{zn}: A tibble of trace element Zn with 873 rows and 12 variables
 #' @docType data
 #' @keywords dataset trace elements
 "zn"
 
 #' @title Spatial grid data of explanatory variables.
 #'
-#' @description Spatial grid data of explanatory variables.
+#' @description Spatial grid data of explanatory variables,modified from `geosimilarity`
+#' package.
 #'
 #' @name grid
-#' @format \code{grid}: A data frame of grided trace element explanatory variables
-#' with 13132 rows and 12 variables, where the first column is ID.
+#' @format \code{grid}: A tibble of grided trace element explanatory variables
+#' with 13132 rows and 12 variables, where the first column is GridID.
 #' @docType data
 #' @keywords dataset trace elements
 "grid"
--- R/gos.R
+++ R/gos.R
@@ -1,57 +1,47 @@
-#' Geographically optimal similarity
+#' @title Geographically optimal similarity
+#' @author Wenbo Lv \email{lyu.geosocial@gmail.com}
+#' @description
+#' Computationally optimized function for geographically optimal similarity (GOS) model
 #'
-#' @description Function for geographically optimal similarity (GOS) model
+#' @usage gos(formula, data = NULL, newdata = NULL, kappa = 0.25, cores = 1)
 #'
-#' @usage gos(formula, data = NULL, newdata = NULL, kappa = 0.25)
-#'
-#' @param formula A formula of GOS model
-#' @param data A data.frame of observation data
-#' @param newdata A data.frame of prediction variables data
+#' @param formula A formula of GOS model.
+#' @param data A data.frame or tibble of observation data.
+#' @param newdata A data.frame or tibble of prediction variables data.
 #' @param kappa A numeric value of the percentage of observation locations
 #'              with high similarity to a prediction location.
 #'              kappa = 1 - tau, where tau is the probability parameter
 #'              in quantile operator. The default kappa is 0.25, meaning
 #'              that 25% of observations with high similarity to a prediction
 #'              location are used for modelling.
+#'@param cores positive integer(default is 1). If cores > 1, a 'parallel' package
+#'cluster with that many cores is created and used. You can also supply a cluster object
 #'
-#' @return A list of predictions and uncertainties.
+#' @return A tibble made up of predictions and uncertainties.
 #'
 #' @importFrom stats as.formula sd quantile
-#' @importFrom SecDim rmvoutlier
+#' @importFrom parallel makeCluster stopCluster parLapply
+#' @importFrom tibble as_tibble
+#' @importFrom purrr map_dfr
 #'
 #' @examples
-#' data("zn")
-#' # log-transformation
-#' hist(zn$Zn)
-#' zn$Zn = log(zn$Zn)
-#' hist(zn$Zn)
-#' # remove outliers
-#' require(SecDim)
-#' k = rmvoutlier(zn$Zn, coef = 2.5)
-#' dt = zn[-k,]
-#' # split data for validation: 70% training; 30% testing
-#' split = sample(1:nrow(dt), round(nrow(dt)*0.7))
-#' train = dt[split,]
-#' test = dt[-split,]
-#' system.time({ # 0.33s
-#' g1 = gos(Zn ~ Slope + Water + NDVI  + SOC + pH + Road + Mine,
-#'            data = train, newdata = test, kappa = 0.25)
-#' })
-#' test$pred = g1$pred
-#' plot(test$Zn, test$pred)
-#' cor(test$Zn, test$pred)
+#' \dontrun{
+#' g = gos(Zn ~ Slope + Water + NDVI  + SOC + pH + Road + Mine,
+#' data = zn, newdata = grid, kappa = 0.08,cores = 6)
+#' g
+#' }
+
 #' @export
 
-
-gos = \(formula, data = NULL, newdata = NULL, kappa = 0.25){
+gos = \(formula, data = NULL, newdata = NULL, kappa = 0.25, cores = 1){
+  doclust = FALSE
   tau = 1 - kappa
-
   formula = as.formula(formula)
   response = data[,colnames(data) == as.character(formula[[2]])]
   response = as.vector(as.matrix(response))
-
   no = nrow(data)
   np = nrow(newdata)
+
   if (formula[[3]]=="."){
     nv = ncol(data) - 1
   } else {
@@ -70,48 +60,50 @@ gos = \(formula, data = NULL, newdata = NULL, kappa = 0.25){
     pred_explanatory = subset(newdata, TRUE, match(all.vars(formula)[-1], colnames(newdata)))
   }
 
+  if (inherits(cores, "cluster")) {
+    doclust = TRUE
+  } else if (cores > 1) {
+    doclust = TRUE
+    cores = parallel::makeCluster(cores)
+    on.exit(parallel::stopCluster(cores), add=TRUE)
+  }
+
   Ej = function(xobs, xp1, snp, sdv){
     sdj = sqrt(sum((xp1 - xobs)^2)/snp)
     ej = exp(-(xp1 - xobs)^2/(2*(sdv*sdv/sdj)^2))
     return(ej)
   }
 
-
   obs_explanatory = as.matrix(obs_explanatory)
   pred_explanatory = as.matrix(pred_explanatory)
   xall = rbind(obs_explanatory, pred_explanatory)
   sdv = sapply(1:nv, function(x) sd(xall[,x]))
 
-
-  zp1 = c()
-  uczeta = c(0.9, 0.95, 0.99, 0.995, 0.999, 1)
-  uncertaintyj = data.frame(matrix(NA, np, 6))
-  names(uncertaintyj) = uczeta
-
-
-  for (u in 1:np){
-    yobs = response
-    xobsmatrix = obs_explanatory
+  calculgos = \(u){
     xpredvalues = pred_explanatory[u,]
-
-    ej1 = lapply(1:nv, function(x) Ej(xobsmatrix[,x], xpredvalues[x], np, sdv[x]))
+    ej1 = lapply(1:nv, function(x) Ej(obs_explanatory[,x], xpredvalues[x], np, sdv[x]))
     ej = do.call(pmin, ej1)
-
     k = which(ej >= quantile(ej, tau))
     ej2 = ej[k]
 
-    zp1[u] = sum(yobs[k] * ej2) / sum(ej2)
-
-    # uncertaintyj = 1 - max(ej)
-    uncertaintyj[u, 1] = 1 - quantile(ej2, 0.9)
-    uncertaintyj[u, 2] = 1 - quantile(ej2, 0.95)
-    uncertaintyj[u, 3] = 1 - quantile(ej2, 0.99)
-    uncertaintyj[u, 4] = 1 - quantile(ej2, 0.995)
-    uncertaintyj[u, 5] = 1 - quantile(ej2, 0.999)
-    uncertaintyj[u, 6] = 1 - quantile(ej2, 1)
+    c(sum(response[k] * ej2) / sum(ej2),
+      1 - quantile(ej2, 0.9),
+      1 - quantile(ej2, 0.95),
+      1 - quantile(ej2, 0.99),
+      1 - quantile(ej2, 0.995),
+      1 - quantile(ej2, 0.999),
+      1 - quantile(ej2, 1)) -> pred_unce
+    names(pred_unce) = c('pred',paste0('uncertainty',
+                                       c(90, 95, 99, 99.5, 99.9, 100)))
+    return(pred_unce)
   }
 
-  out = list("pred" = zp1, "uncertainty" = uncertaintyj)
+  if (doclust) {
+    out = parallel::parLapply(cores, 1:np, calculgos)
+    out = tibble::as_tibble(do.call(rbind, out))
+  } else {
+    out = purrr::map_dfr(1:np,calculgos)
+  }
+
   return(out)
 }
-
