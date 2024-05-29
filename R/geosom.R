240816bcaee19279ebe0604a2365bd98424fa4ea
SpatLyu <lyu.geosocial@gmail.com>
2024-05-30 00:02:49 +0800

    fix bug of geosom model

--- R/geosom.R
+++ R/geosom.R
@@ -234,8 +234,8 @@ geosom_bestsuperclass = \(gsom, nclass= 2,method = "pam",
     ok.hclust = stats::hclust(stats::dist(codebook), hmethod)
 
   ncells = nrow(codebook)
-  nvalues = max(nclass, min(ncells, 59, max(ceiling(sqrt(ncells)),
-                            length(unique(gsom$unit.classif)))))
+  nvalues = max(nclass, min(ncells, max(ceiling(sqrt(ncells)),
+                            (length(unique(gsom$unit.classif)) - 1))))
   clust.var = sapply(1:nvalues, function(k) {
     if (method == "hclust") {
       clust = stats::cutree(ok.hclust, k)
